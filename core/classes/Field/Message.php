19dc8e22c6d63dac8eb4b14f8fccbcd6412e97aa
Radoslav Georgiev <rageorgiev@gmail.com>
2018-05-15 13:46:07 +0200

    [FIX] Messages are not saving values anymore.

--- core/classes/Field/Message.php
+++ core/classes/Field/Message.php
@@ -17,4 +17,15 @@ class Message extends Field {
 	public function enqueue_scripts() {
 		wp_enqueue_script( 'uf-field-message' );
 	}
+
+	/**
+	 * Ensures that unlike normal fields, no values are saved for messages.
+	 *
+	 * @since 3.0.2
+	 *
+	 * @param mixed[] $source The source which the value of the field would be available in.
+	 */
+	public function save( $source ) {
+		// Nothing to do here really...
+	}
 }
